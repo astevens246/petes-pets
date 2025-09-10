a51eec89301b7b1af599f79401cc60ea924d209e
Allen Stevens <allen.stevens@students.dominican.edu>
2025-09-10 11:34:24 -0700

    Users can search pets by name and breed

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ const Pet = require('../models/pet');
 
 module.exports = (app) => {
 
-  /* GET home page. */
-  app.get('/', (req, res) => {
-    Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
-    });
-  });
-}
+// SEARCH PET
+app.get('/search', (req, res) => {
+  term = new RegExp(req.query.term, 'i')
+
+  Pet.find({$or:[
+    {'name': term},
+    {'species': term}
+  ]}).exec((err, pets) => {
+    res.render('pets-index', { pets: pets });
+  })
+});}

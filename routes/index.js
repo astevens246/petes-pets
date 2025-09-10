7516105a752b3b4b3107f280cfa9b39362ae685d
Allen Stevens <allen.stevens@students.dominican.edu>
2025-09-10 11:52:43 -0700

    Implemented pagination on the home page

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,24 @@ const Pet = require('../models/pet');
 
 module.exports = (app) => {
 
+/* GET home page. */
+app.get('/', (req, res) => {
+  const page = req.query.page || 1;
+  
+  Pet.paginate({}, {page: page}).then((results) => {
+    res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });    
+  });
+});
+
 // SEARCH PET
 app.get('/search', (req, res) => {
-  term = new RegExp(req.query.term, 'i')
+  const term = new RegExp(req.query.term, 'i');
 
   Pet.find({$or:[
     {'name': term},
     {'species': term}
   ]}).exec((err, pets) => {
-    res.render('pets-index', { pets: pets });
-  })
-});}
+    res.render('pets-index', { pets: pets });    
+  });
+});
+}

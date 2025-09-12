755ad8e536254f4b196403af5d7d9df248385ae7
Allen Stevens <allen.stevens@students.dominican.edu>
2025-09-12 11:13:13 -0700

    Implemented S3 file uploads

--- routes/pets.js
+++ routes/pets.js
@@ -1,3 +1,30 @@
+// UPLOADING TO AWS S3
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  },{
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
+
 // MODELS
 const Pet = require('../models/pet');
 
@@ -12,17 +39,44 @@ module.exports = (app) => {
   });
 
   // CREATE PET
-  app.post('/pets', (req, res) => {
+  app.post('/pets', upload.single('avatar'), (req, res, next) => {
+    console.log(req.file)
     var pet = new Pet(req.body);
+  pet.save(function (err) {
+    if (err) { 
+      console.log('Validation error:', err.errors);
+      return res.status(400).send(err.errors); 
+    }
+    
+    if (req.file) {
+      // Upload the images
+      client.upload(req.file.path, {}, function (err, versions, meta) {
+        if (err) { 
+          console.log('S3 upload error:', err);
+          return res.status(400).send({ err: err }) 
+        };
 
-    pet.save()
-      .then((pet) => {
-        res.send(pet);
-      })
-      .catch((err) => {
-        // STATUS OF 400 FOR VALIDATIONS
-        res.status(400).send(err.errors);
-      }) ;
+        // Pop off the -square and -standard and just use the one URL to grab the image
+        versions.forEach(function (image) {
+          var urlArray = image.url.split('-');
+          urlArray.pop();
+          var url = urlArray.join('-');
+          pet.avatarUrl = url;
+        });
+        
+        // Save the pet with the avatarUrl after setting it
+        pet.save(function(err) {
+          if (err) {
+            console.log('Error saving pet with avatarUrl:', err);
+            return res.status(400).send({ err: err });
+          }
+          res.send({ pet: pet });
+        });
+      });
+    } else {
+      res.send({ pet: pet });
+    }
+  })
   });
 
   // SHOW PET
